d0a1c0443a025819ffd87bce4e0208944770dcbd
vivekbhatt07 <vivekbhatt618@gmail.com>
2022-03-31 15:37:21 +0530

    Added: Components

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main>
+      <div>
+        <input></input>
+        <input></input>
+      </div>
+      <button></button>
+      <img src="/" alt="change" />
+    </main>
+  );
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="right-nav">
+        <img src="/" />
+        <span>Meme Generator</span>
+      </div>
+      <div className="left-nav">
+        <span>React Course - Project 3</span>
+      </div>
+    </div>
+  );
+}
